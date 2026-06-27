364a9616677c88c5938010c19145dea8b2b405fc
Zweihander-25 <aknioumohamedachraf@gmail.com>
2026-06-27 15:55:08 +0100

    added editorRowInsertChar and editorInsertChar for inserting characters in the file we can now type texts not only view text

--- grimoire.c
+++ grimoire.c
@@ -219,6 +219,25 @@ void editorAppendRow(char *s, size_t len) {
     E.numrows++; // Increment the number of rows in the editor
 }
 
+void editorRowInsertChar(erow *row, int at, int c){ //insert a single character into erow at a given position
+    if(at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/** editor operations ***/ 
+
+void editorInsertChar(int c){ //will take a character and use editorRowInsertChar to insert that char into the position that the cursor is at
+    if(E.cy == E.numrows){
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -340,6 +359,9 @@ void editorProcessKeypress() {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c); // Move the cursor based on the key pressed
+            break;
+        default:
+            editorInsertChar(c);
             break;
     }
 }
