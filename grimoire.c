5d4a05dc91137f5dd618db8b264d84458985b101
Zweihander-25 <aknioumohamedachraf@gmail.com>
2026-07-03 00:17:28 +0100

    save as.. feature now if the user runs ./grimoire with no arguments, they get a blank file to edit and can save that file by prompting a name for it and save it

--- grimoire.c
+++ grimoire.c
@@ -66,6 +66,8 @@ struct editorConfig E; // Global editor configuration
 /*** prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** terminal***/
 
@@ -358,7 +360,13 @@ void editorOpen(char *filename) {
 }
 
 void editorSave(){
-    if(E.filename == NULL) return;
+    if(E.filename == NULL){
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if(E.filename == NULL){
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
     
     int len;
     char *buf = editorRowsToString(&len);
@@ -403,6 +411,40 @@ void abFree(struct abuf *ab) {
 }
 
 /*** input ***/
+
+char *editorPrompt(char *prompt){
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while(1){
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if(c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE){
+            if(buflen != 0) buf[--buflen] = '\0';
+        }else if(c == '\x1b'){
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        }else if(c == '\r'){
+            if(buflen != 0){
+                editorSetStatusMessage("");
+                return buf;
+            }
+        }else if(!iscntrl(c) && c<128){
+            if(buflen == bufsize -1){
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorMoveCursor(int key) {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -542,7 +584,7 @@ void editorDrawRows(struct abuf *ab) {
         if(filerow >= E.numrows) {
             if(E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
-                int welcomelen = snprintf(welcome, sizeof(welcome), "THE GRIMOIRE editor -- version %s", THE_GRIMOIRE_VER); // Create a welcome message
+                int welcomelen = snprintf(welcome, sizeof(welcome), "GRIMOIRE editor -- version %s", THE_GRIMOIRE_VER); // Create a welcome message
                 if(welcomelen > E.screencols) welcomelen = E.screencols; // Truncate the message if it exceeds the screen width
                 int padding = (E.screencols - welcomelen) / 2; // Calculate padding to center the message
                 if(padding) {
